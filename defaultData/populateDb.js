41ba61f848359fe683b23df731fae4aa55c583a6
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-09 15:18:58 -0800

    working on schemas and populateDb

--- defaultData/populateDb.js
+++ defaultData/populateDb.js
@@ -1,3 +1,20 @@
 const settings = require('../settings.json');
 const mongoose = require('mongoose');
+const categories = require('./categoriesDefaultData.json');
+const products = require('./productsDefaultData.json');
 
+function main() {
+  console.log('This should populate the database with fields from the default data json')
+  const dbUrl = settings.dbUrl + '/' + settings.dbName;
+  console.log('attempting to connect to mongo db')
+  connectToMongo(dbUrl);
+}
+
+async function connectToMongo(url) {
+  try {
+    await mongoose.connect(url);
+    console.log(`Connected to db: ${url}`)
+  } catch (err) {
+    console.error(err);
+  }
+}
