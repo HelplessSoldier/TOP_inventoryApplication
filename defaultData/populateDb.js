c3c2d7f1ab0dfc0433b052789b69dcc0b3ec9222
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-09 15:33:42 -0800

    added more to populatedb

--- defaultData/populateDb.js
+++ defaultData/populateDb.js
@@ -5,11 +5,14 @@ const Category = require('../models/category');
 const products = require('./productsDefaultData.json');
 const Product = require('../models/product');
 
-function main() {
+async function main() {
   console.log('This should populate the database with fields from the default data json')
   const dbUrl = settings.dbUrl + '/' + settings.dbName;
   console.log('attempting to connect to mongo db')
   connectToMongo(dbUrl);
+  console.log('adding data')
+  await createCategories();
+  await createProducts();
 }
 
 async function connectToMongo(url) {
@@ -20,3 +23,28 @@ async function connectToMongo(url) {
     console.error(err);
   }
 }
+
+async function createCategories() {
+  for (let category of categories) {
+    const newCategory = new Category({
+      name: category.name,
+    });
+    await newCategory.save();
+    console.log(`added category: ${category.name}`)
+  }
+}
+
+async function createProducts() {
+  for (let product of products) {
+    const newProduct = new Product({
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      price: product.price,
+      numInStock: product.numInStock
+    });
+    await newProduct.save();
+    console.log(`added product: ${product.name}`);
+  }
+}
+
