02690c91873096bc7188f7ec27ebe36019573168
HelplessSoldier <patrick1234143211@gmail.com>
2023-11-10 13:04:34 -0800

    basic product detail page

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,3 +9,8 @@ exports.products_in_category = asyncHandler(async (req, res, next) => {
   res.render('products_in_category', { products_list: allProductsInCategory, title: req.params.name })
 })
 
+exports.product_detail = asyncHandler(async (req, res, next) => {
+  const itemId = req.params.id;
+  const item = await Product.findById(itemId);
+  res.render('product_detail', { item });
+})
--- routes/stock.js
+++ routes/stock.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const category_controller = require('../controllers/categoryController');
 const product_controller = require('../controllers/productController');
 
-router.get('/categories', category_controller.category_list)
-router.get('/category/:name', product_controller.products_in_category)
+router.get('/categories', category_controller.category_list);
+router.get('/category/:name', product_controller.products_in_category);
+router.get('/product/:id', product_controller.product_detail);
 
 module.exports = router;
